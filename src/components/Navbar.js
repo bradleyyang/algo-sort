46b5dc684594c53edadc804b85dfba05ea6f634c
bradleyyang <bradley.yang023@gmail.com>
2024-03-25 14:50:01 -0400

    edited navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,21 +4,24 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <div>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/InsertionSort">Insertion Sort</Link>
-            </li>
-            <li>
-                <Link to="/SelectionSort">Selection Sort</Link>
-            </li>
-            <li>
-                <Link to="/MergeSort">Merge Sort</Link>
-            </li>
-            <li>
-                <Link to="/QuickSort">Quick Sort</Link>
-            </li>
+            <ul className="navbar">
+                <li className="block">
+                    <Link to="/">Home</Link>
+                </li>
+                <li className="block">
+                    <Link to="/InsertionSort">Insertion Sort</Link>
+                </li>
+                <li className="block">
+                    <Link to="/SelectionSort">Selection Sort</Link>
+                </li>
+                <li className="block">
+                    <Link to="/MergeSort">Merge Sort</Link>
+                </li>
+                <li className="block">
+                    <Link to="/QuickSort">Quick Sort</Link>
+                </li>
+            </ul>
+            
         </div>
     )
 }
